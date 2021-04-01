359041f1766f06a8d18677b832187708708aa716
SavileLee <58343733+SavileLee@users.noreply.github.com>
2021-04-01 11:43:15 +0800

    Update LinuxNET.sh

--- LinuxNET.sh
+++ LinuxNET.sh
@@ -344,7 +344,7 @@ fi
 
 [ -n "$ipAddr" ] && [ -n "$ipMask" ] && [ -n "$ipGate" ] && setNet='1';
 [[ -n "$tmpWORD" ]] && myPASSWORD="$(openssl passwd -1 "$tmpWORD")";
-[[ -z "$myPASSWORD" ]] && myPASSWORD='$1$4BJZaD0A$y1QykUnJ6mXprENfwpseH0';
+[[ -z "$myPASSWORD" ]] && myPASSWORD='$1$4BJZaD0A$bn4SVa35BgZNP0HlEGwIC.';
 
 if [[ -n "$interface" ]]; then
   IFETH="$interface"
