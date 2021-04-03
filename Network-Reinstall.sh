2aad2a8b41abc91f8cdf062ceaed77dc39bc2c28
SavileLee <58343733+SavileLee@users.noreply.github.com>
2021-04-03 19:51:29 +0800

    Update Network-Reinstall.sh

--- Network-Reinstall.sh
+++ Network-Reinstall.sh
@@ -6,20 +6,21 @@
 ## BLOG: https://dreamstart.site
 
 
-echo -e "\n\n\n"
-clear
-echo -e "\n"
-echo "---------------------------------------------------------------------------------------------------------------------"
-echo -e "\033[33m Network-Reinstall-System Tools\033[0m"
-echo -e "\033[33m Reinstall the Linux system requires only network and one click \033[0m"
-echo -e "\033[33m System requirements: Any Linux system with GRUB or GRUB2, recommended CentOS8/Debian10/Ubuntu20 \033[0m"
-echo -e "\n"
-echo -e "\033[33m [Original] One-click Network Reinstall System\033[0m"
-echo -e "\033[33m https://savilelee.github.io/Network/ \033[0m"
-echo "---------------------------------------------------------------------------------------------------------------------"
-echo " Default root password: dreamstart.site"
-echo "---------------------------------------------------------------------------------------------------------------------"
-echo -e "\n"
+function CopyRight() {
+  clear
+  echo "                                                           "
+  echo "================================================================"
+  echo "=                                                              ="
+  echo "=           一键网络重装系统 （图形化安装）                    ="
+  echo "=        Network-Reinstall-System (Graphical Install)          ="
+  echo "=                                                              ="
+  echo "=                                https://www.dreamstart.site   ="
+  echo "=                                                              ="
+  echo "================================================================"
+  echo " Default root password: dreamstart.site"
+  echo "================================================================"
+  echo -e "\n"
+}
 
 if [[ $EUID -ne 0 ]]; then
 	1>&2
@@ -64,145 +65,12 @@ echo "--------------------------------------------------------------------------
 echo -e "\n"
 sleep 1s
 
-function CopyRight() {
-  clear
-  echo "                                                           "
-  echo "================================================================"
-  echo "=                                                              ="
-  echo "=           一键网络重装系统 （图形化安装）                    ="
-  echo "=        Network-Reinstall-System (Graphical Install)          ="
-  echo "=                                                              ="
-  echo "=                                https://www.dreamstart.site   ="
-  echo "=                                                              ="
-  echo "================================================================"
-  echo -e "\n"
-}
 
-function isValidIp() {
-  local ip=$1
-  local ret=1
-  if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-    ip=(${ip//\./ })
-    [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
-    ret=$?
-  fi
-  return $ret
-}
-
-function ipCheck() {
-  isLegal=0
-  for add in $MAINIP $GATEWAYIP $NETMASK; do
-    isValidIp $add
-    if [ $? -eq 1 ]; then
-      isLegal=1
-    fi
-  done
-  return $isLegal
-}
-
-function GetIp() {
-  MAINIP=$(ip route get 1 | awk -F 'src ' '{print $2}' | awk '{print $1}')
-  GATEWAYIP=$(ip route | grep default | awk '{print $3}' | head -1)
-  SUBNET=$(ip -o -f inet addr show | awk '/scope global/{sub(/[^.]+\//,"0/",$4);print $4}' | head -1 | awk -F '/' '{print $2}')
-  value=$(( 0xffffffff ^ ((1 << (32 - $SUBNET)) - 1) ))
-  NETMASK="$(( (value >> 24) & 0xff )).$(( (value >> 16) & 0xff )).$(( (value >> 8) & 0xff )).$(( value & 0xff ))"
-}
-
-function UpdateIp() {
-  read -r -p "Your IP: " MAINIP
-  read -r -p "Your Gateway: " GATEWAYIP
-  read -r -p "Your Netmask: " NETMASK
-}
-
-function SetNetwork() {
-  isAuto='0'
-  if [[ -f '/etc/network/interfaces' ]];then
-    [[ ! -z "$(sed -n '/iface.*inet static/p' /etc/network/interfaces)" ]] && isAuto='1'
-    [[ -d /etc/network/interfaces.d ]] && {
-      cfgNum="$(find /etc/network/interfaces.d -name '*.cfg' |wc -l)" || cfgNum='0'
-      [[ "$cfgNum" -ne '0' ]] && {
-        for netConfig in `ls -1 /etc/network/interfaces.d/*.cfg`
-        do
-          [[ ! -z "$(cat $netConfig | sed -n '/iface.*inet static/p')" ]] && isAuto='1'
-        done
-      }
-    }
-  fi
-
-  if [[ -d '/etc/sysconfig/network-scripts' ]];then
-    cfgNum="$(find /etc/network/interfaces.d -name '*.cfg' |wc -l)" || cfgNum='0'
-    [[ "$cfgNum" -ne '0' ]] && {
-      for netConfig in `ls -1 /etc/sysconfig/network-scripts/ifcfg-* | grep -v 'lo$' | grep -v ':[0-9]\{1,\}'`
-      do
-        [[ ! -z "$(cat $netConfig | sed -n '/BOOTPROTO.*[sS][tT][aA][tT][iI][cC]/p')" ]] && isAuto='1'
-      done
-    }
-  fi
-}
-
-function NetMode() {
-  CopyRight
-
-  if [ "$isAuto" == '0' ]; then
-    read -r -p "Using DHCP to configure network automatically? [Y/n]:" input
-    case $input in
-      [yY][eE][sS]|[yY]) NETSTR='' ;;
-      [nN][oO]|[nN]) isAuto='1' ;;
-      *) NETSTR='' ;;
-    esac
-  fi
-
-  if [ "$isAuto" == '1' ]; then
-    GetIp
-    ipCheck
-    if [ $? -ne 0 ]; then
-      echo -e "Error occurred when detecting ip. Please input manually.\n"
-      UpdateIp
-    else
-      CopyRight
-      echo "IP: $MAINIP"
-      echo "Gateway: $GATEWAYIP"
-      echo "Netmask: $NETMASK"
-      echo -e "\n"
-      read -r -p "Confirm? [Y/n]:" input
-      case $input in
-        [yY][eE][sS]|[yY]) ;;
-        [nN][oO]|[nN])
-          echo -e "\n"
-          UpdateIp
-          ipCheck
-          [[ $? -ne 0 ]] && {
-            clear
-            echo -e "Input error!\n"
-            exit 1
-          }
-        ;;
-        *) clear; echo "Canceled by user!"; exit 1;;
-      esac
-    fi
-    NETSTR="--ip-addr ${MAINIP} --ip-gate ${GATEWAYIP} --ip-mask ${NETMASK}"
-  fi
-}
-
-function Start() {
-  CopyRight
-
-  if [ "$isAuto" == '0' ]; then
-    echo "Using DHCP mode."
-  else
-    echo "IP: $MAINIP"
-    echo "Gateway: $GATEWAYIP"
-    echo "Netmask: $NETMASK"
-    sleep 3s
-  fi
 
     if [ -f "/tmp/Core_Install.sh" ]; then
     rm -f /tmp/Core_Install.sh
   fi
   wget --no-check-certificate -qO /tmp/Core_Install.sh 'https://savilelee.github.io/Network/CoreFiles/LinuxNET.sh' && chmod a+x /tmp/Core_Install.sh
-
-  sed -i "/18.04/a\        [[ \"\$isDigital\" == \'20.04\' ]] && DIST=\'focal\'\;" /tmp/Core_Install.sh
-  sed -i '/force-efi-extra-removable/d' /tmp/Core_Install.sh
 
 echo -e "\n\n\n"
 clear
@@ -248,9 +116,9 @@ read N
   	1) echo -e "\nPassword: dreamstart.site\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/Core_Install.sh  -c 8 -v 64 -a -firmware ;;
     2) echo -e "\nPassword: dreamstart.site\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/Core_Install.sh -c 7 -v 64 -a -firmware ;;
     3) echo -e "\nPassword: dreamstart.site\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/Core_Install.sh -c 6.10 -v 64 -a -firmware ;;
-    4) echo -e "\nPassword: dreamstart.site\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/Core_Install.sh -d 10 -v 64 -a -firmware $NETSTR ;;
+    4) echo -e "\nPassword: dreamstart.site\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/Core_Install.sh -d 10 -v 64 -a -firmware ;;
     5) echo -e "\nPassword: dreamstart.site\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/Core_Install.sh -d 10 -v 64 -a -firmware ;;
-    6) echo -e "\nPassword: dreamstart.site\n"; read -s -n1 -p "Press any key to continue..." ; sed -i 's#images/netboot#legacy-images/netboot#' /tmp/InstallNET.sh; bash /tmp/Core_Install.sh -u 20.04 -v 64 -a ;;
+    6) echo -e "\nPassword: dreamstart.site\n"; read -s -n1 -p "Press any key to continue..." ; sed -i 's#images/netboot#legacy-images/netboot#' /tmp/Core_Install.sh; bash /tmp/Core_Install.sh -u 20.04 -v 64 -a ;;
     7) echo -e "\nPassword: dreamstart.site\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/Core_Install.sh -u 18.04 -v 64 -a ;;
     8) echo -e "\nPassword: dreamstart.site\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/Core_Install.sh -u 16.04 -v 64 -a ;;
     9)
