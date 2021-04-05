1c91d6ea3ed8a73d3267ff3a9e2929dfdd80ea98
SavileLee <58343733+SavileLee@users.noreply.github.com>
2021-04-05 20:01:04 +0800

    Update Network-Reinstall.sh

--- Network-Reinstall.sh
+++ Network-Reinstall.sh
@@ -356,6 +356,9 @@ function MENU() {
     8) Ubuntu_16.04;;
     9)
       echo -e "\n"
+      CopyRight
+      Install_start
+      line
       read -r -p "请输入系统镜像地址 Custom image URL: " imgURL
       read -s -n1 -p "按任意键继续,Ctrl+C退出... Press any key to continue, Ctrl+C to Exit..." ; 
       bash /tmp/Core_Install.sh -dd "$imgURL" ;;
